ffb1991c6f7f5cde19e053556cb32d06c55a32d1
Qing Shi <44493311+sqsssq@users.noreply.github.com>
2024-04-13 22:35:34 +0800

    Remove skip relax method

---
+++ assets/Login.76215229.js
@@ -0,0 +1,1 @@
+import{l as e}from"./login.6c687cd9.js";import{_ as t,r as n,o as s,c,a as r}from"./index.f28ead6f.js";const a={data(){return{}},components:{loginVue:e},computed:{},mounted(){},methods:{}},i={style:{height:"100%",width:"100%"}};function p(_,d,m,l,u,f){const o=n("loginVue");return s(),c("div",i,[r(o)])}const V=t(a,[["render",p]]);export{V as default};
